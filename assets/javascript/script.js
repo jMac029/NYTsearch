0acb8011092f8d5a2b9445661ab2c4444675f3e6
James McMillan <jmcmillan29@gmail.com>
2017-11-04 13:49:34 -0700

    search is working needs styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,51 +1,59 @@
 // Custom Javascript to use the NYT API
 
+$(document).ready(function(){
+
 // copied from the NYT API Documentation
 
-//var searchTerm = $('#searchTerm').val().trim();
+var searchTerm;
+// console.log(searchTerm);
+
+	console.log("nytimes search")
+
+	$('#search-button').click(function() { 
+		event.preventDefault();
+		searchTerm = $('#search-term').val();
+		console.log(searchTerm);
+		articleSearch();
+	});
+
+	function articleSearch() {
+
+		var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+		url += '?' + $.param({
+		  'api-key': "be0c4c568c9e4f62ab24e5b17f92bb97",
+		  'q': searchTerm,
+			});
+		$.ajax({
+		  url: url,
+		  method: 'GET',
+			}).done(function(result) {
+
+		  		var response = result.response.docs;
+
+		  		console.log(response);
+
+		  		
+
+		  		for (var i = 0; i < response.length; i++) {
+
+		  			var headline = response[i].headline.main;
+			  		var link = response[i].web_url;
+			  		var snip = response[i].snippet;
+			  		//var byLine = response[i].byline.original;
+		  			var articleDiv = $('<div>');
 
 
+		  			$(articleDiv).append(headline, link, snip);
+		  			$('#search-results').append(articleDiv);
 
-$('#search-button').click(function() { 
-	event.preventDefault();
-	articleSearch();
+		  		}
+
+
+			}).fail(function(err) {
+		  		throw err;
+			});
+
+	}
 
 });
-
-function articleSearch() {
-
-	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-
-	url += '?' + $.param({
-	  'api-key': "be0c4c568c9e4f62ab24e5b17f92bb97",
-	  'q': "Anaheim Ducks" // searchTerm
-		});
-	$.ajax({
-	  url: url,
-	  method: 'GET',
-		}).done(function(result) {
-
-	  		var response = result.response.docs[i];
-
-	  		console.log(response);
-
-	  		var headline = response.headline.main;
-	  		var link = response.web_url;
-	  		var snip = response.snippet;
-	  		var byLine = response.byline.original;
-
-	  		for (var i = 0; i < response.length; i++) {
-	  			var articleDiv = $('<div>');
-
-
-	  			$(articleDiv).append(headline, snip, byLine);
-	  			$('#search-results').append(articleDiv);
-
-	  		}
-
-
-		}).fail(function(err) {
-	  		throw err;
-		});
-
-}
